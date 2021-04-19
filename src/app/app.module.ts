2876ab00a8c153ffc324250b2f157fc3daf7e089
Andrea <andrea.accorinti186@gmail.com>
2021-04-19 11:54:11 +0200

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { F2Component } from './f2/f2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    F2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
